cd76b35d781a3e14ea4518b0e1417b5044e57238
Juan Pablo Casarino <juan21casarino@gmail.com>
2023-10-28 13:45:03 -0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Test from './Test';
+import './app.scss';
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id='Homepage'>
+      <Navbar/>
+      <Hero/>
+    </section>
+    <section id='Services'>Parallax</section>
+    <section>Services</section>
+    <section id='Portfolio'>Parallax</section>
+    <section>Portfolio</section>
+    <section>Portfolio</section>
+    <section>Portfolio</section>
+    <section id='About'>Portfolio</section>
+    <section id='Contact'>Contact</section>
+  </div>
 };
 
 export default App;
