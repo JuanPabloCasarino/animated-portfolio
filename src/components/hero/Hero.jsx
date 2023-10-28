73dedb3503addb1e2997a4fbf511985ef07f41a0
Juan Pablo Casarino <juan21casarino@gmail.com>
2023-10-28 16:06:31 -0300

    Agrego el hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,42 @@
 import './hero.scss'
+import { motion } from 'framer-motion'
+const textVariants = {
+  initial: {
+    x:-500,
+    opacity:0,
+  },
+  animate:{
+    x:0,
+    opacity:1,
+    transition:{
+      duration:1,
+      staggerChildren:0.1,
+    },
+  },
+  scrollButton:{
+    opacity:0,
+    y:10,
+    transition:{
+      delay:1,
+      duration:2,
+      repeat:Infinity
+    }
+  }
+}
 
 const Hero = () => {
   return (
     <div className='hero'>
-      Hero
+      <div className="wrapper">
+        <motion.div className='text_pres' variants={textVariants} initial='initial' animate='animate'>
+          <motion.h2 variants={textVariants}>JUAN PABLO CASARINO</motion.h2>
+          <motion.h3 variants={textVariants}>Full Stack <br/>Web Developer</motion.h3>
+          <motion.button className='button' variants={textVariants}>Contact me</motion.button>
+          <motion.img src='/scroll.png' variants={textVariants} animate="scrollButton"></motion.img>
+        </motion.div>
+      </div>
+      <div className="slidingTextContainer">Javascript NodeJS React.js HTML CSS MongoDB</div>
+      
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
                 <motion.div initial={{opacity:0, scale:0.5}}
                 animate={{opacity:1,scale:1}}
                 transition={{duration:0.5}} className="social">
-                    <a href=""><img src='/instagram.png' alt='Facebook'></img></a>
-                    <a href=""><img src='/github.png' alt='Facebook'></img></a>
-                    <a href=""><img src='/youtube.png' alt='Facebook'></img></a>
+                    <a href=""><motion.img src='/instagram.png' alt='Instagram' whileHover={{scale:1.5}} whileTap={{scale:0.95}}></motion.img></a>
+                    <a href=""><motion.img src='/github.png' alt='Github' whileHover={{scale:1.5}} whileTap={{scale:0.95}}></motion.img></a>
+                    <a href=""><motion.img src='/linkedin.png' alt='Linkedin' whileHover={{scale:1.5}} whileTap={{scale:0.95}}></motion.img></a>
                 </motion.div>
             </div>
         </div>
